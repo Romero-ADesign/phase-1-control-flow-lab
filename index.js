695c76c6507d0e10e102c099d0e90453afec7b54
Adam Romero <aromero0904@gmail.com>
2022-12-13 00:38:22 -0500

    commit tree 2e9b48ce7051f86f21f00db48460a9c9e267abae

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    const price = `This one is on me!`;
+    return price;
+  } else if (distance > 401 && distance < 2000 ) {
+    const price = 'That will be twenty bucks.';
+    return price;
+  } else if (distance > 2000 && distance < 2500) {
+    const price = `I will gladly take your thirty bucks.`
+    return price;
+  } else if (distance >= 2500) {
+    const price = `No can do.`;
+    return price;
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(destination){
+  if (destination === `NYC`) {
+    const response = `Ok, sounds good.`;
+    return response;
+  } else {
+    const response = `No go.`;
+    return response;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let goodbye;
+
+  switch(tip) {
+    case `generous`:
+      goodbye = `Thank you so much.`
+      break;
+    case `not as generous`:
+      goodbye = `Thank you.`
+      break;
+    default:
+      goodbye = `Bye.`
+      break;
+  }
+  return goodbye;
 }
